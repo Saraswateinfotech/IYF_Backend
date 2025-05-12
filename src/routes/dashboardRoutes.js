ddcd2ecb8727a9639249cf58556e4255d7934b55
Liladhar Kumawat <liladharkumawat007@gmail.com>
2025-05-12 21:16:30 +0530

    add get 3 top Frontliner api

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
-const { getDashboardReport, getFrontlinerdetailReport } = require("../controllers/dashboardController");
+const { getDashboardReport, getFrontlinerdetailReport, getTop3FrontlinersByMonthYear } = require("../controllers/dashboardController");
 
 
 
 router.get('/dashboard-report',authMiddleware, getDashboardReport);
 router.get('/frontliner-report/:callingId',authMiddleware, getFrontlinerdetailReport);
 
+router.get('/top-3-frontliners/:month/:year', authMiddleware, getTop3FrontlinersByMonthYear);
 
 module.exports = router;
